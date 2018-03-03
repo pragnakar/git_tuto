567590a7b7360c3eac8869fe507a92b47ccbc12b
pragn <pragnakar@gmail.com>
2018-03-03 12:54:09 -0500

    added test python file

---
+++ test_pytho.py
@@ -0,0 +1,11 @@
+class first_class():
+
+    def __init__(self):
+        print("inititalizing a class")
+
+    def first_function(self):
+        print('first function')
+
+if __name__ == '__main__':
+    obj = first_class()
+    obj.first_function()
