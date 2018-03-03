db32ccd1d14b96dd443be24a32eeb5efe4d714da
pragn <pragnakar@gmail.com>
2018-03-03 18:01:34 -0500

    added third function

--- test_pytho.py
+++ test_pytho.py
@@ -5,9 +5,13 @@ class first_class():
 
     def first_function(self):
         print('first function')
+    def third_function(self,a,b):
+            return a*b
 
 if __name__ == '__main__':
     obj = first_class()
     obj.first_function()
     print("this is a third branch")
     # push from atom
+    print(obj.third_function())
+    
