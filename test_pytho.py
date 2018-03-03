2f65f89c92e5408a92089b9e4cbb42a555978c48
pragn <pragnakar@gmail.com>
2018-03-03 13:03:32 -0500

    add a commit to branch

--- test_pytho.py
+++ test_pytho.py
@@ -5,7 +5,10 @@ class first_class():
 
     def first_function(self):
         print('first function')
-
+    def function_in_branch(self):
+        print("this is a function in branch")
+        return None
 if __name__ == '__main__':
     obj = first_class()
     obj.first_function()
+    obj.function_in_branch()
